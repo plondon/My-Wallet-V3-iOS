97aa709a5533426659d476d0a1e3ed4fb1072869
woudini <wukl011@gmail.com>
2017-02-09 11:16:37 -0500

    feat(Buy): initialize buy view controller on app launch

--- Blockchain/BuyBitcoinViewController.m
+++ Blockchain/BuyBitcoinViewController.m
@@ -13,53 +13,38 @@
 
 @interface BuyBitcoinViewController () <WKNavigationDelegate, WKScriptMessageHandler>
 @property (nonatomic) WKWebView *webView;
-
-@property (nonatomic) NSString *guid;
-@property (nonatomic) NSString *sharedKey;
-@property (nonatomic) NSString *password;
 @end
 
 @implementation BuyBitcoinViewController
 
-- (id)initWithGuid:(NSString *)guid sharedKey:(NSString *)sharedKey password:(NSString *)password
+- (id)init
 {
     if (self = [super init]) {
-        self.guid = guid;
-        self.sharedKey = sharedKey;
-        self.password = password;
+        
+        WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
+        WKUserContentController* userController = [[WKUserContentController alloc] init];
+        
+        [userController addScriptMessageHandler:self name:@"buyCompleted"];
+        
+        configuration.userContentController = userController;
+        
+        self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, DEFAULT_HEADER_HEIGHT, self.view.frame.size.width, self.view.frame.size.height - DEFAULT_HEADER_HEIGHT) configuration:configuration];
+        [self.view addSubview:self.webView];
+        
+        self.webView.navigationDelegate = self;
+        self.automaticallyAdjustsScrollViewInsets = NO;
+        
+        NSURL *login = [NSURL URLWithString:@"http://localhost:8080/wallet/#/intermediate"];
+        NSURLRequest *request = [NSURLRequest requestWithURL:login cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval: 10.0];
+        [self.webView loadRequest:request];
+        
     }
     return self;
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
-    WKUserContentController* userController = [[WKUserContentController alloc] init];
-
-    [userController addScriptMessageHandler:self name:@"buyCompleted"];
-    
-    configuration.userContentController = userController;
-    
-    self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, DEFAULT_HEADER_HEIGHT, self.view.frame.size.width, self.view.frame.size.height - DEFAULT_HEADER_HEIGHT) configuration:configuration];
-    [self.view addSubview:self.webView];
-
-    self.webView.navigationDelegate = self;
-    self.automaticallyAdjustsScrollViewInsets = NO;
-
-    NSURL *login = [NSURL URLWithString:@"http://localhost:8080/wallet/#/intermediate"];
-    NSURLRequest *request = [NSURLRequest requestWithURL:login cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval: 10.0];
-    [self.webView loadRequest:request];
-}
-
-- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
+- (void)loginWithGuid:(NSString *)guid sharedKey:(NSString *)sharedKey password:(NSString *)password
 {
-    [self performSelector:@selector(login) withObject:nil afterDelay:3];
-}
-
-- (void)login
-{
-    NSString *function = [NSString stringWithFormat:@"activateMobileBuy(\"%@\", \"%@\", \"%@\")", [self.guid escapeStringForJS], [self.sharedKey escapeStringForJS], [self.password escapeStringForJS]];
+    NSString *function = [NSString stringWithFormat:@"activateMobileBuy(\"%@\", \"%@\", \"%@\")", [guid escapeStringForJS], [sharedKey escapeStringForJS], [password escapeStringForJS]];
     [self.webView evaluateJavaScript:function completionHandler:^(id _Nullable result, NSError * _Nullable error) {
         DLog(@"Login result %@, error %@", result, error);
     }];
